bd037ba68ed0922af2b5f5a92cda1c24aabb8d35
unknown <tran.viet.hoang844@gmail.com>
2021-11-08 11:59:20 +0700

    first commit

---
+++ src/main/java/com/duan/kingfragrance/KingfragranceApplication.java
@@ -0,0 +1,13 @@
+package com.duan.kingfragrance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingfragranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingfragranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duan/kingfragrance/KingfragranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duan.kingfragrance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingfragranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
